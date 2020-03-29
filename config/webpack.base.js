e35b8758325ab8d492065b19877c5a4b8a85cf80
sumarina <marinzoo@hotmail.com>
2020-03-29 17:57:52 +0800

    增加量化速度插件

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -2,16 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const SpeedMeasureWebpackPlugin = require('speed-measure-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 const isDev = process.env.NODE_ENV === 'development';
 const config = require('../public/config')[isDev ? 'dev' : 'build'];
 
-module.exports = {
+const webpackConfig = {
   entry: './src/index.js',
   resolve: {
-    extensions: ['.js', '.jsx'],
-    modules: ['./src/components', 'node_modules'] //resolve配置webpack如何寻找模块所对应的文件
+    extensions: ['.js', '.jsx']
+    // modules: ['./src/components', 'node_modules'] //resolve配置webpack如何寻找模块所对应的文件 注意坑，如果引入的模块中也存在node_modules，则会提示找不到对应模块
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
@@ -22,7 +23,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        use: ['babel-loader'],
+        use: ['cache-loader', 'babel-loader'], //在耗时较长的loader前添加cache-loader，缓存结果，节省性能
         // options: {
         //   presets: ['@babel/preset-env'],
         //   plugins: [
@@ -34,7 +35,8 @@ module.exports = {
         //     ]
         //   ]
         // },
-        exclude: /node_modules/ //排除node_modules文件
+        include: [path.resolve(__dirname, '../src')]
+        // exclude: /node_modules/ //排除node_modules文件
       },
       {
         test: /\.(le|c)ss$/,
@@ -55,7 +57,8 @@ module.exports = {
           },
           'less-loader'
         ],
-        exclude: /node_module/
+        include: [path.resolve(__dirname, '../src/css')]
+        // exclude: /node_module/
       },
       {
         test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
@@ -70,7 +73,8 @@ module.exports = {
             }
           }
         ],
-        exclude: /node_module/
+        include: [path.resolve(__dirname, '../src/img')]
+        // exclude: /node_module/
       }
     ]
   },
@@ -98,3 +102,5 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ]
 };
+
+module.exports = new SpeedMeasureWebpackPlugin().wrap(webpackConfig);
